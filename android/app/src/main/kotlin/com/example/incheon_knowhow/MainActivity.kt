8104462acdba2567ecb956a7ca6843f8565aad02
HunHee <yhh1125@gmail.com>
2023-12-30 23:39:19 +0900

    impl app icon & launcher

--- android/app/src/main/kotlin/com/example/incheon_knowhow/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/incheon_knowhow/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.incheon_knowhow
 
 import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        intent.putExtra("background_mode", "transparent")
-        super.onCreate(savedInstanceState)
-    }
+class MainActivity: FlutterFragmentActivity() {
+    
 }
